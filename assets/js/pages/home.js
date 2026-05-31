ed42b82f77d2f0364b6a18d8c2c7318111630bbd
mohenz <mohenz@hotmail.com>
2026-06-01 00:11:13 +0900

    feat: remove main featured movies section from home page

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -67,7 +67,6 @@
       </div>`;
 
     sections.innerHTML = [
-      mainFeatured.length ? UI.movieSection("메인 전시작", "관리자가 선택한 메인 전시 작품입니다.", mainFeatured) : "",
       UI.movieSection("최신등록 8개", "최근 등록된 영화정보입니다.", latest),
       UI.movieSection("맞춤추천 8개", "클릭수가 높은 영화정보입니다.", recommended),
       UI.movieSection("카테고리별 평가등급 상위 8개", "랭킹 점수와 등급을 함께 반영했습니다.", topRated)
